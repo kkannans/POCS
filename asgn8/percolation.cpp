eca85eb89e0c7604d0360b3a6bf87fa0c9f18f0d
Krishna Kannan Srinivasan <kkannans@uvm.edu>
2022-10-21 20:43:53 -0400

    running with percolation.py

--- asgn8/percolation.cpp
+++ asgn8/percolation.cpp
@@ -1,69 +1,61 @@
-#include<iostream>
-#include<vector>
-#include<unordered_map>
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <stdlib.h>
 
-// This function will return the number of connected cells around the starting [i,j]
-int dfs(vector<vector<int> >& visited,vector<vector<int>>& grid,int i,int j){
-	if(i<0 || j<0 || i>=grid.size() || j>=grid[i].size()){
-		return 0; // If we go out of the matrix, then no connected cell
-    }
+// int maxAreaOfIsland(vector<vector<int>>&grid){
+//         int Max=0;
+//          int row = grid.size();
+//     int col = grid[0].size();
+//     for(int i=0;i<row;i++){
+//         for(int j=0;j<col;j++){
+//                int  cnt=1;
+//             if( grid[i][j]==1){
+//                 dfs(grid, i, j, cnt);
+//                Max = max(Max, cnt);  }  
+//         }
+//     }
+//        return Max;
+// }
 
-    // If the cell has been already visited or if the cell is marked as 0, then don't
-    // need to visit this cell.
-    if(visited[i][j]==true || grid[i][j]==0){
-        return 0;
-    }
+// int dfs(vector<vector<int>>& grid, int i, int j, int& cnt){
+//     if( i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0){
+//     return 0;
+//     }
+//     grid[i][j]=0;
+//     if(dfs(grid, i+1,j, cnt))cnt++;
+//         if(dfs(grid, i-1,j, cnt))cnt++;
+//         if(dfs(grid, i,j-1, cnt))cnt++;
+//         if(dfs(grid, i,j+1, cnt))cnt++;
+//     return 1;
+// }
+double randZeroToOne(){
+    return rand() / (RAND_MAX + 1.);
+};
 
-    visited[i][j]=true;
+std::vector<std::vector<int> create_grid(int size){
+    std::cout << size << "\n " << p;
+    std::vector<std::vector<int>> result(size, std::vector<int>(size, 0));
+    // std::generate(result.begin(), result.end(), [](float p) {
+    // return randZeroToOne() <= p;
+    // });
+    return result;
+};
 
-    // Travel on the connected cells
-    int connectedCells = 1;
-    connectedCells += dfs(visited,grid,i+1,j);
-    connectedCells += dfs(visited,grid,i-1,j);
-    connectedCells += dfs(visited,grid,i,j+1);
-    connectedCells += dfs(visited,grid,i,j-1);
-    return connectedCells;
-}
 
-// This function will return the frequency of each size of a connected grid present in the matrix
-unordered_map<int,int> findFrequency(vector<vector<int>>& grid) {
-    unordered_map<int,int> ans;
+int main() {
 
-    // First making a visited array to maintain the cells already visited during the DFS call.
-    vector<vector<int> > visited(
-    	grid.size(),
-    	vector<int>(grid[0].size(),false)
-    );
-
-    for(int i=0;i<grid.size();i++){
-        for(int j=0;j<grid[i].size();j++){
-            if(visited[i][j]==false && grid[i][j]==1){
-            	// Count the number of cells in this connected 
-            	// grid and update its size in the hashmap
-                int count = dfs(visited,grid,i,j);
-                ans[count]++;
-            }
-        }
-    }
-
-    return ans;
-}
-
-int main(){
-	vector<vector<int> > grid{
-		{1,1,1,1,1,1},
-		{1,1,0,0,0,0},
-		{0,0,0,1,1,1},
-		{0,0,0,1,1,1},
-		{0,0,1,0,0,0},
-		{1,0,0,0,0,0},
-	};
-
-	vector<int> queries{6 , 1 , 8, 2};
-
-	unordered_map<int,int> frequencyCount = findFrequency(grid);
-	for(int i=0;i<queries.size();i++){
-		cout<<frequencyCount[queries[i]]<<endl;
-	}
+    double p = 0.5;
+    // std::cout << p ;
+    // std::vector<std::vector<int> g = create_grid(10, p);
+    // std::cout << grid.size();
+    // for (int i = 0; i < grid.size(); i++)
+    // {
+    // for (int j = 0; j < grid[i].size(); j++)
+    // {
+    //     std::cout << grid[i][j];
+    // }
+    // }
+    return 0;
 }
--- asgn8/percolation.py
+++ asgn8/percolation.py
@@ -1,69 +1,78 @@
 # https://codereview.stackexchange.com/questions/210641/size-of-the-largest-connected-component-in-a-grid-in-python
-from collections import deque
+# https://leetcode.com/problems/max-area-of-island/solution/
 import numpy as np
 import random
-from collections import OrderedDict
 import pickle
+from pprint import pprint
+
+def maxAreaOfIsland(grid):
+    seen = set()
+    ans = 0
+    for r0, row in enumerate(grid):
+        for c0, val in enumerate(row):
+            if val and (r0, c0) not in seen:
+                shape = 0
+                stack = [(r0, c0)]
+                seen.add((r0, c0))
+                while stack:
+                    r, c = stack.pop()
+                    shape += 1
+                    for nr, nc in ((r-1, c), (r+1, c), (r, c-1), (r, c+1)):
+                        if (0 <= nr < len(grid) and 0 <= nc < len(grid[0])
+                                and grid[nr][nc] and (nr, nc) not in seen):
+                            stack.append((nr, nc))
+                            seen.add((nr, nc))
+                ans = max(ans, shape)
+    return ans
 
 def largest_connected_component(grid):
-    EMPTY = 0
-    FILLED = 1
-    VISITED = 2
+    """Find largest connected component of 1s on a grid."""
 
-    def is_valid(x, y):
-        return (0 <= x < len(grid) and 0 <= y < len(grid[0]) and
-                grid[x][y] != VISITED)
-
-    def traverse_component(x, y):
-        grid[x][y] = VISITED
+    def traverse_component(pos):
+        """Returns no. of unseen elements connected to (i,j)."""
+        i, j = pos
         result = 1
-        for adjacent in ((x + 1, y),
-                         (x - 1, y),
-                         (x, y + 1),
-                         (x, y - 1)):
-            if (is_valid(*adjacent)):
-                if (grid[adjacent[0]][adjacent[1]] == EMPTY):
-                    q.append(adjacent)
-                    grid[adjacent[0]][adjacent[1]] = VISITED
-                else:
-                    result += traverse_component(*adjacent)
 
+        # Check all four neighbours
+        for new_pos in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+            if new_pos in elements:
+                elements.remove(new_pos)
+                result += traverse_component(new_pos)
         return result
 
-    max_filled_size = 0
-    q = deque()
-    if (grid[0][0] == EMPTY):
-        q.append((0, 0))
-        grid[0][0] = VISITED
-    else:
-        max_filled_size = max(max_filled_size, traverse_component(0, 0))
+    # Tracks size of largest connected component found
+    elements = set((i, j) for i, line in enumerate(grid) for j, cell in enumerate(line) if cell)
+    largest = 0
+    while elements:
+        pos = elements.pop()
+        largest = max(largest, traverse_component(pos))
+    return largest
 
-    while q:
-        x, y = q.popleft()
+def generate_grid(grid_size:int, prob:float):
+    lattice_rand = np.random.rand(grid_size,grid_size)
+    lattice = (lattice_rand < prob).astype(int)
+    return lattice
 
-        for adjacent in ((x + 1, y),
-                         (x - 1, y),
-                         (x, y + 1),
-                         (x, y - 1)):
-            if (is_valid(*adjacent)):
-                if (grid[adjacent[0]][adjacent[1]] == EMPTY):
-                    q.append(adjacent)
-                    grid[adjacent[0]][adjacent[1]] = VISITED
-                else:  # FILLED
-                    max_filled_size = max(max_filled_size, traverse_component(*adjacent))
-
-    return max_filled_size
-
-L = [20, 50, 100, 200, 500, 1000]
-p_vals = [round(i*0.01,4) for i in range(0,100)]
-# for each L run 100 experiments corresponding to different p_vals
-lcc_L = {round(k,2): {v: 0} for k in L for v in p_vals}
-for grid_size in L:
-    print("gridsize = ", grid_size)
-    for p in p_vals:
-        lcc_L[grid_size][p] = largest_connected_component(generate_grid(grid_size, p))
-        lcc_L[grid_size] = {k:v for k,v in sorted(lcc_L[grid_size].items(), key=lambda t: t[0])}
-
-exp_file = open('percolation_experiment.pickle', 'wb')
-pickle.dump(lcc_L, exp_file)
-exp_file.close()
+if __name__ == "__main__":
+    # test
+    # grid = generate_grid(10, 0.1)
+    # pprint(grid)
+    # lcc = maxAreaOfIsland(grid)
+    # print(lcc)
+    L = [20, 50, 100, 200, 500, 1000]
+    p_vals = [round(i*0.01,4) for i in range(0,100)]
+    num_experiments = 100
+    # for each L run 100 experiments corresponding to p in p_vals
+    for grid_size in L:
+        print("Running 100 experiments for gridsize = ", grid_size)
+        lcc_L = { k:[] for k in p_vals}
+        for p in p_vals:
+            # perform 100 experiments and compute the fractional component size ~ lcc/grid_size^2
+            for _ in range(num_experiments):
+                lcc = maxAreaOfIsland(generate_grid(grid_size, p))
+                fractional_size = lcc/(grid_size*grid_size)
+                lcc_L[p].append(fractional_size)
+            lcc_L = {k:v for k,v in sorted(lcc_L.items(), key=lambda t: t[0])}
+            exp_file = open('percolation_experiment_' + str(grid_size)+'.pickle', 'wb')
+            pickle.dump(lcc_L, exp_file)
+            exp_file.close()
